2e582cc7c62a5c8317d648bd703cd34dffeb2a24
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2022-09-09 19:10:10 -0300

    creación de primeros componentes - header y navbar

---
+++ src/components/menu/Header.jsx
@@ -0,0 +1,15 @@
+import '../header.css'
+import logo from '../../logosolosvg.svg';
+import NavBar from './NavBar';
+
+function Header(){
+  return(
+    <header className="Header">
+    <img src={logo} className="App-logo" alt="logo" />
+    <NavBar/>
+    </header>
+
+  );
+}
+
+export default Header;
---
+++ src/components/menu/NavBar.jsx
@@ -0,0 +1,14 @@
+function NavBar(){
+return(
+<nav className="NavBar">
+  <ul class="menu">
+    <li><a href="home" alt="menu">Home</a></li>
+    <li><a href="http://#" alt="menu">Productos</a></li>
+    <li><a href="http://#" alt="menu">Contacto</a></li>
+  </ul>
+</nav>
+
+);
+}
+
+export default NavBar;
